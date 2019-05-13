2cbdd17216e86917346196080f373b12a21575fd
Tanmaya Patra <tanmayapatra09@gmail.com>
2019-05-13 10:06:22 +0530

    Sample sliders are addes to home page and some changes are done to the form firlds

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -11,6 +11,27 @@ export class Tab1Page implements OnInit {
     slidesPerView: '1',
     spaceBetween: 25,
   };
+
+  slideConfigTwo = {
+    slidesPerView: '3.2',
+    spaceBetween: 6,
+  };
+
+  slideConfigThree = {
+    slidesPerView: '1.2',
+    spaceBetween: 5,
+  };
+
+  slideConfigFour = {
+    slidesPerView: '1.4',
+    spaceBetween: 6,
+  };
+
+  slideConfigCategorySlides = {
+    slidesPerView: '2.2',
+    spaceBetween: 6,
+  };
+  
   navCtrl: any;
   constructor(private router: Router) {
   }
